fd3179a01ac0510aad164f3e51416eaa69d64614
eevgeniy <evlevgenij@yandex.ru>
2023-06-16 14:39:24 +0400

    задачи 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (Максимум 100000)
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int number = new Random().Next(100000);
+if (number < 100)
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+}
+else
+{
+    int number1 = number % 1000;
+    int number2 = number1 / 100;
+    Console.WriteLine($"{number} -> {number2}");
+}
